3046e0ae5f2f1703d4bdb6e755b246c00160a755
Basilis031104 <129120354+Basilis031104@users.noreply.github.com>
2025-08-06 18:29:24 +0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCHjsbAQcjiTv9obNpwjb3JF0a59-D7uhI",
+  authDomain: "smartcashly.firebaseapp.com",
+  projectId: "smartcashly",
+  storageBucket: "smartcashly.firebasestorage.app",
+  messagingSenderId: "381329374069",
+  appId: "1:381329374069:web:f5000a5e781af3fd45b13c",
+  measurementId: "G-PMBQ3QLS7S"
+};
